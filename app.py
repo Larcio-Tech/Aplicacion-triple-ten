f73e606bde24fe0e7ef8fc956a94962d38469038
Larcio-Tech <bazookakid15@gmail.com>
2026-08-16 01:50:29 -0600

    fix live chart

--- app.py
+++ app.py
@@ -5,25 +5,32 @@ import time
 
 st.header('Lanzar una moneda')
 
-chart = st.line_chart(pd.DataFrame([0.5]))
+chart_placeholder = st.empty()
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
     outcome_no = 0
     outcome_1_count = 0
+    means = [0.5]
 
     for r in trial_outcomes:
         outcome_no += 1
+
         if r == 1:
             outcome_1_count += 1
+
         mean = outcome_1_count / outcome_no
-        chart.add_rows(pd.DataFrame([mean]))
+        means.append(mean)
+
+        chart_placeholder.line_chart(pd.DataFrame(means))
         time.sleep(0.05)
 
     return mean
+
+chart_placeholder.line_chart(pd.DataFrame([0.5]))
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
